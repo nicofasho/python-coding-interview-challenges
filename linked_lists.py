b8e341704e49f5fcade3ebef149b418b8d96664b
Christian Watson <chris23w@gmail.com>
2019-08-05 11:52:03 -0700

    linked list 2.6

--- linked_lists.py
+++ linked_lists.py
@@ -100,4 +100,68 @@ def delete_middle_node(node):
   node.next = n.next
   return True
 
-def partition(list, )
+def partition(list, x):
+  # my answer, almost, but not quite, doesn't put lesser values at head, and greater values at tail to ensure partition
+  # n = list.head
+  # while n != None:
+  #   if n.data < x:
+  #     n.next = n
+  
+  # correct answer
+  n = list.head
+  while n != None:
+    if n.data < x:
+      list.head.next = list.head
+      list.head = n
+    else:
+      n.append_to_tail(n.data)
+
+def sum_lists(l1, l2):
+  n1 = l1.head
+  n2 = l2.head
+
+  while n1 != None:
+    if n2 == None:
+      return False
+    s = n1.data + n2.data
+    l3 = LinkedList(Node(s))
+    n3 = l3.head
+    if s > 9:
+      if n1.next != None:
+        s = s - 10
+        n3.next = Node(s)
+        n1.next.data += 1
+      else:
+        s = s - 10
+        n3.next = Node(s)
+        n1.next = Node(1)
+        n2.next = Node(0)
+    else:
+      n3.next = Node(s)
+      n1 = n1.next
+      n2 = n2.next
+      n3 = n3.next
+
+  n3 = l3.head
+  sum_list = []
+  while n3 != None:
+    sum_list.append(n3.data)
+    n3 = n3.next
+
+  sum_list.reverse()
+  sum_str = ''.join(map(str, sum_list))
+  return sum_str
+
+def is_palindrome(l):
+  n = l.head
+  str = []
+
+  while n != None:
+    str.append(n.data)
+    n = n.next
+  
+  if str == str.reverse():
+    return True
+  else:
+    return False
+
